7666e59ef86f805671913b623898b5517fbe918e
erdem220509 <126973925+erdem220509@users.noreply.github.com>
2023-04-21 20:21:16 +0300

    new

--- index.js
+++ index.js
@@ -1,58 +1,20 @@
-const textarea = document.querySelector("textarea"),
-    voiceList = document.querySelector("select"),
-    speechBtn = document.querySelector("button");
-
-let synth = speechSynthesis,
-    isSpeaking = true;
-
-voices();
-
-function voices() {
-    for (let voice of synth.getVoices()) {
-        let selected = voice.name === "Google US English" ? "selected" : "";
-        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
-        voiceList.insertAdjacentHTML("beforeend", option);
+// Butona tıklanınca çalışacak fonksiyonu tanımla
+function login() {
+    // Kullanıcı adı ve şifre inputlarının değerlerini al
+    var username = document.querySelector(".input1 input").value;
+    var password = document.querySelector(".input2 input").value;
+  
+    // Kullanıcı adı ve şifrenin doğruluğunu kontrol et
+    if (username === "admin" && password === "password") {
+      // Giriş başarılı mesajını göster
+      alert("Giriş başarılı!");
+      window.location.href = "voice.html"
+    } else {
+      // Kullanıcı adı veya şifre yanlış ise hata mesajı göster
+      alert("Kullanıcı adı veya şifre yanlış!");
     }
-}
-
-synth.addEventListener("voiceschanged", voices);
-
-function textToSpeech(text) {
-    let utterance = new SpeechSynthesisUtterance(text);
-    for (let voice of synth.getVoices()) {
-        if (voice.name === voiceList.value) {
-            utterance.voice = voice;
-        }
-    }
-    synth.speak(utterance);
-}
-
-speechBtn.addEventListener("click", e => {
-    e.preventDefault();
-    if (textarea.value !== "") {
-        // Checks if not speaking, Speak Textarea Text
-        if (!synth.speaking) {
-            textToSpeech(textarea.value);
-        }
-        // If text was long, Add Resume and Pause Function
-        if (textarea.value.length > 80) {
-            setInterval(() => {
-                if (!synth.speaking && !isSpeaking) {
-                    isSpeaking = true;
-                    speechBtn.innerText = "Convert To Speech";
-                } else { }
-            }, 500);
-            if (isSpeaking) {
-                synth.resume();
-                isSpeaking = false;
-                speechBtn.innerText = "Pause Speech";
-            } else {
-                synth.pause();
-                isSpeaking = true;
-                speechBtn.innerText = "Resume Speech";
-            }
-        } else {
-            speechBtn.innerText = "Convert To Speech";
-        }
-    }
-});
+  }
+  
+  // Butona tıklanınca login fonksiyonunu çalıştır
+  document.querySelector(".button button").addEventListener("click", login);
+  
