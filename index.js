7f4c5a1ca5fcc73ced0861fa6de79e7df3e294c8
erdem220509 <126973925+erdem220509@users.noreply.github.com>
2023-07-04 21:18:04 +0300

    new design bugs fixed

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-// Butona tıklanınca çalışacak fonksiyonu tanımla
 function login() {
-    // Kullanıcı adı ve şifre inputlarının değerlerini al
-    var username = document.querySelector("#username").value;
-    var password = document.querySelector("#password").value;
-  
-    // Kullanıcı adı ve şifrenin doğruluğunu kontrol et
-    if (username === "admin" && password === "password") {
-      // Giriş başarılı mesajını göster
-      alert("Giriş başarılı!");
-      window.location.href = "main.html"
-    } else {
-      // Kullanıcı adı veya şifre yanlış ise hata mesajı göster
-      alert("Kullanıcı adı veya şifre yanlış!");
-    }
+  var username = document.querySelector("#username").value;
+  var password = document.querySelector("#password").value;
+
+  if (username === "admin" && password === "password") {
+    alert("Login Succesfully");
+    window.location.href = "main.html";
+  } else {
+    alert("Username or Password incorrect");
   }
-  
-  // Butona tıklanınca login fonksiyonunu çalıştır
-  document.querySelector("#button").addEventListener("click", login);
-  
+}
+
+document.querySelector("#button").addEventListener("click", login);
